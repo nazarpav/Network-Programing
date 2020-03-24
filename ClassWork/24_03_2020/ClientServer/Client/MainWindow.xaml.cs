384d747bc987776caa246549187fe67687f5a184
павлюк назар <hasab.k18@gmail.com>
2020-03-24 12:02:37 +0200

    fp

---
+++ ClassWork/24_03_2020/ClientServer/Client/MainWindow.xaml.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Client
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            IPAddress ip = IPAddress.Parse("127.0.0.1"); //Dns.GetHostAddresses("microsoft.com")[0];
+            IPEndPoint ep = new IPEndPoint(ip, 1024);
+
+            Socket s = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            try
+            {
+                s.Connect(ep);
+                if (s.Connected)
+                {
+                    String strSend = TB.Text;
+                    s.Send(System.Text.Encoding.ASCII.GetBytes(strSend));
+                    byte[] buffer = new byte[1024];
+                    int l;
+                    do
+                    {
+                        l = s.Receive(buffer);
+                        LV.Items.Add(System.Text.Encoding.ASCII.GetString(buffer, 0, l));
+                    } while (l > 0);
+                }
+                else
+                    MessageBox.Show("Connection Error");
+            }
+            catch (SocketException ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            finally
+            {
+                s.Shutdown(SocketShutdown.Both);
+                s.Close();
+            }
+        }
+    }
+}
